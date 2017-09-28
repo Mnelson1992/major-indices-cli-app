50ef14a0e47d1c3bdc6af51f5770176f7aa8309c
Matthew Nelson <nelson.matthew2015@gmail.com>
2017-09-28 02:08:30 +0000

    Added text to my CLI file

--- lib/major_indices/cli.rb
+++ lib/major_indices/cli.rb
@@ -1,8 +1,26 @@
 class MajorIndices::CLI
-
+#Make a CLI with fake data just to get a basic structure of how I want it to work
   def call
-    puts "Welcome to Major Indices!"
-  end 
+    greeting
+    menu
+    goodbye
+  end
+
+  def greeting
+    puts "Welcome to the Major Indices CLI gem!"
+    puts "This gem you will give you the 3 major U.S. Indices:"
+    puts "    The Dow, NASDAQ, and S&P 500"
+    puts "With this gem you can see the current value of each of the indices,"
+    puts "the net change, and the percent change."
+  end
+
+  def menu
+    puts "Here is a menu"
+  end
+
+  def goodbye
+    puts "Goodbye!"
+  end
 
 
 
