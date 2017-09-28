cec8ac5a1fdf7a2dc32ad5ee9fb7bf55831b9168
Matthew Nelson <nelson.matthew2015@gmail.com>
2017-09-28 23:44:44 +0000

    Addedthe bones of the CLI

--- lib/major_indices/cli.rb
+++ lib/major_indices/cli.rb
@@ -1,28 +1,66 @@
 class MajorIndices::CLI
-#Make a CLI with fake data just to get a basic structure of how I want it to work
+
+# 1. code ClI with fake data to make sure that it works
   def call
     greeting
     menu
-    goodbye
   end
 
   def greeting
-    puts "Welcome to the Major Indices CLI gem!"
-    puts "This gem you will give you the 3 major U.S. Indices:"
-    puts "    The Dow, NASDAQ, and S&P 500"
-    puts "With this gem you can see the current value of each of the indices,"
-    puts "the net change, and the percent change."
+    puts"Welcome to the Major Indices Gem!"
   end
 
   def menu
-    puts "Here is a menu"
+    puts "Please choose from the following menu:"
+    puts "    To display the indices, their current values and percent change, type 1"
+    puts "    To display list of just the indices, type 2"
+    puts "    To display list of the indices and their current values, type 3"
+    puts "    To display the indices and their percent change, type 4"
+    puts "    To exit, type 'exit'"
+    input = gets.strip
+
+      if input == '1'
+        display_info
+        option
+      elsif input =='2'
+        display_indices
+        option
+      elsif input == '3'
+        current_value
+        option
+      elsif input == '4'
+        percent
+        option
+      else
+        puts "That is an invalid entry, please try again"
+        menu
+      end
   end
 
-  def goodbye
-    puts "Goodbye!"
+  def option
+    puts "Would you like to view any more data? (Y/N)"
+    input = gets.strip.upcase
+      if input == "Y"
+        menu
+      elsif input == "N"
+        puts "See ya later!"
+      end
+    end
+
+  def display_info
+    puts "index - value - percent"
   end
 
+  def display_indices
+    puts "i. index" #sorted alphabetically
+  end
 
+  def current_value
+    puts "index - value" #sorted alphabetically
+  end
 
+  def percent
+    puts "index - percent" #sorted alphabetically
+  end
 
 end
