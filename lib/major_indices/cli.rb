41d8418362c28b430f8c82265482ab2c0c56293e
Matthew Nelson <nelson.matthew2015@gmail.com>
2017-09-30 19:09:46 +0000

    added my to cruise.rb file

--- lib/major_indices/cli.rb
+++ lib/major_indices/cli.rb
@@ -1,4 +1,29 @@
 class MajorIndices::CLI
 
+  def call
+    greeting
+    list_cruises
+    menu
+    goodbye
+  end
+
+  def greeting
+    puts "Having a tough time deciding where to go on vacation?"
+    puts ""
+    puts "Well let us help guide you thinking with this fancy new gem"
+    puts ""
+  end
+
+  def list_cruises
+    puts "cruises" #fill in list_cruises method
+  end
+
+  def menu
+    puts "menu" #fill in menu method
+  end
+
+  def goodbye
+    puts "goodbye" #fill in goodbye method
+  end
 
 end
--- lib/major_indices/cruise.rb
+++ lib/major_indices/cruise.rb
@@ -1,4 +1,16 @@
 class MajorIndices::Cruise
+  attr_accessor :name, :description
 
-  
+  @@all =[]
+
+  def initialize(name, description)
+    @name = name
+    @description = description
+    @@all << self
+  end
+
+  def all
+    @@all
+  end 
+
 end
